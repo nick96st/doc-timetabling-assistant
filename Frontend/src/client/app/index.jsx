71e7e64f57c044af2cc51d1f0f2f9dbd1142eb51
Duy Vu <dtv15@imperial.co.uk>
2017-11-15 03:17:55 +0000

    Finally made it work, by introducing state.value

--- Frontend/src/client/app/index.jsx
+++ Frontend/src/client/app/index.jsx
@@ -9,11 +9,13 @@ import {ReactSelectize, SimpleSelect, MultiSelect} from 'react-selectize';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.value;
     this.state = {timetable: [  {time:12, day:"Monday", room: "308", name:"Architecture", type: "lecture"},
                                 {time:13, day:"Monday", room: "308", name:"Architecture", type: "lecture"},
                                 {time:16, day:"Tuesday", room: "311", name:"Hardware", type: "lecture"},
                                 {time:17, day:"Tuesday", room: "311", name:"Hardware", type: "lecture"},
-                                {time:12, day:"Wednesday", room: "308", name:"Databases I", type: "lecture"},]};
+                                {time:12, day:"Wednesday", room: "308", name:"Databases I", type: "lecture"},],
+                              value: null};
                               //{time: 11, day: "Monday", room: "311", name: "Advanced Databases", type: "lecture"},
                               //{time: 12, day: "Monday", room: "311", name: "Advanced Databases", type: "lecture"},
                              // {time: 9, day: "Monday", room: "311", name: "Operations Research", type: "lecture"},
@@ -112,12 +114,46 @@ class App extends React.Component {
     return rooms
   }
 
+  filterTable(data, rooms){
+    var rs = []
+    rooms.forEach(r => {rs.push(r.value)})
+    var monday = {day: "Monday" }
+    var tuesday = {day: "Tuesday"}
+    var wednesday = {day: "Wednesday"}
+    var thursday = {day: "Thursday"}
+    var friday = {day: "Friday"}
+    console.log("rs is " + rs);
+    data.forEach(d => {if(rs.indexOf(d.room) !== -1){
+                         if(d.day === "Monday"){monday[d.time] = d.name + " " + d.room}
+                         if(d.day === "Tuesday"){tuesday[d.time] = d.name + " " + d.room}
+                         if(d.day === "Wednesday"){wednesday[d.time] = d.name + " " + d.room}
+                         if(d.day === "Thursday"){thursday[d.time] = d.name + " " + d.room}
+                         if(d.day === "Friday"){friday[d.time] = d.name + " " + d.room}
+                       }
+                       });
+    var rows = [monday, tuesday, wednesday, thursday, friday]
+    console.log(tuesday)
+    return rows
+  }
+
+
   render () {
-    var rows = this.generateRows(this.state.timetable)
-    var timetable = <Timetable rows={rows}/>
+    var rows
+    var timetable
+    console.log("GETHERE ??????1st")
+    if(this.state.value == null) {
+      rows = this.generateRows(this.state.timetable)
+      timetable = <Timetable rows={rows}/>
+    } else {
+      rows = this.filterTable(this.state.timetable, this.state.value)
+      timetable = <Timetable rows={rows}/>
+      console.log("GETHERE ??????")
+    }
+
     var saveBtn = <button onClick={ () => {this.saveTimetable(this.state.timetable)}}>Save</button>
     var checkBtn = <button onClick={ () => {this.checkTimetable(this.state.timetable)}}>Check</button>
     var generateBtn = <button onClick={ () => {this.generateTimetable()}}>Generate</button>
+    var refreshBtn = <button onClick={ () => {}}>Refresh</button>
     var rooms = this.getRooms(this.state.timetable)
     // var dropDown = <MultiSelect placeholder="Select a fruit"
     //                             options = {rooms.map(
@@ -125,12 +161,27 @@ class App extends React.Component {
     //                             )}
     //                             onValueChange = {value => alert(value)}
     //                />
+
     var dropDown = <MultiSelect
                     placeholder = "Select room(s)"
                     theme = "material"
                     options = {rooms.map(
                       room => ({label: room, value: room})
                     )}
+                    onValuesChange = {value => {console.log("timetable CHANGEEEE" + value)
+                                                console.log(value)
+                                                console.log("BEFORE " + this.statevalue)
+                                                this.setState({value : value})
+                                                // this.forceUpdate();
+                                                console.log("AFTER " + this.state.value)
+                                                // var newRows = this.filterTable(this.state.timetable, value)
+                                                // var filteredTimetable = <Timetable rows={newRows}/>
+                                                // console.log(filteredTimetable)
+                                                // this.setState({timetable: newRows})
+                                                // this.setState({rows: newRows})
+                                                // console.log(newRows)
+
+                                                }}
                    />
     return( <div>
             {dropDown}
@@ -138,6 +189,7 @@ class App extends React.Component {
             {saveBtn}
             {checkBtn}
             {generateBtn}
+            {refreshBtn}
            </div>)
   }
 
