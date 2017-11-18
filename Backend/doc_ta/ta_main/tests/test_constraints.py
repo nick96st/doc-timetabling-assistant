d06aedc08814ab9a07d7109e930518a72af0131d
Nikolay Yotov <ny1215@ic.ac.uk>
2017-11-18 17:30:02 +0000

    extract test for constraints in separed file for readability

---
+++ Backend/doc_ta/ta_main/tests/test_constraints.py
@@ -0,0 +1,136 @@
+from django import test
+from ta_main import asp_manipulators
+from ta_main import models as ta_models
+from ta_main import asp_code_generator
+from ta_main import views
+from ta_main.tests import database_inits as DatabaseInits
+
+
+def generate_lectureclass_json(name, room, day, hour):
+    return {"time": hour, "name": name, "room": str(room), "day": day}
+
+
+unsatisfiable = "UNSATISFIABLE"
+satisfiable = "SATISFIABLE"
+
+
+def invoke_codegen_sequence_with_facts(grid_objects):
+    codegen = asp_code_generator.CodeGeneratorBuilder()
+    codegen.for_term("Term 1")
+    codegen.with_result_facts(grid_objects)
+    # codegen.with_hard_constraints(hard_constraints)
+    # codegen.with_soft_constraints(soft_constraints)
+    generator = codegen.build()
+    generator.should_generate = True
+    generator.generate_code('test_001.in')
+    views.run_clingo('./test_001.in', './test_001.out')
+    code_result = generator.get_result_status('test_001.out')
+    return code_result
+
+
+class HardConstraintsTest(test.TestCase):
+    # Note: to test constraints do(look test test_forbid_3_consequitive_hours):
+    # 1) def facts an array of  "[ta_models.LectureClass().init_from_json( \
+    #                           generate_lectureclass_json([course_name],[room],[day],[hour])),"
+    # 2) the satisfy/not satisfy result is result = invoke_codegen_sequence_with_facts(facts)
+    def setUp(self):
+        DatabaseInits.init_base()
+        DatabaseInits.GenerateFirstYearsDB()  # fills the db
+        pass
+
+    def each_class_has_enough_hours_per_week(self):
+        pass
+
+    def test_no_3_consequitive_lectures(self):
+        facts = [ta_models.LectureClass().init_from_json( \
+            generate_lectureclass_json("Hardware", "308", "Monday", 10)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Hardware", "308", "Monday", 11)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Hardware", "308", "Monday", 12)),
+        ]
+
+        code_result = invoke_codegen_sequence_with_facts(facts)
+        self.assertEquals(code_result, unsatisfiable)
+
+    def test_2_lectures_of_same_subject_must_be_consequitive_if_in_same_day(self):
+        facts = [ta_models.LectureClass().init_from_json( \
+            generate_lectureclass_json("Hardware", "308", "Monday", 10)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Hardware", "308", "Monday", 12)),
+        ]
+
+        code_result = invoke_codegen_sequence_with_facts(facts)
+        self.assertEquals(code_result, unsatisfiable)
+
+    def test_room_capacity_requirement(self):
+        facts = [ta_models.LectureClass().init_from_json( \
+            generate_lectureclass_json("Hardware", "311", "Monday", 10)),
+        ]
+
+        code_result = invoke_codegen_sequence_with_facts(facts)
+        self.assertEquals(code_result, unsatisfiable)
+
+    def test_force_two_hours_a_day_unless_uneven(self):
+        pass
+
+    def test_forbid_2_subjects_in_same_room_at_a_given_time(self):
+        facts = [ta_models.LectureClass().init_from_json( \
+            generate_lectureclass_json("Hardware", "308", "Monday", 10)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Descrete", "308", "Monday", 10)),
+        ]
+
+        code_result = invoke_codegen_sequence_with_facts(facts)
+        self.assertEquals(code_result, unsatisfiable)
+
+    def test_allow_clashes_only_if_specified(self):
+        clash = ta_models.Clash()
+        clash.subject = ta_models.Subject.objects.get(title="Hardware")
+        clash.subject2 = ta_models.Subject.objects.get(title="Descrete")
+        clash.save()
+
+        facts_with_valid_clash = [ta_models.LectureClass().init_from_json( \
+                        generate_lectureclass_json("Hardware", "308", "Monday", 10)),
+                        ta_models.LectureClass().init_from_json( \
+                            generate_lectureclass_json("Descrete", "311", "Monday", 10)),
+                                 ]
+        facts_with_invalid_clash = [ta_models.LectureClass().init_from_json( \
+                        generate_lectureclass_json("Hardware", "308", "Monday", 10)),
+                        ta_models.LectureClass().init_from_json( \
+                            generate_lectureclass_json("Math Methods", "311", "Monday", 10)),
+                                 ]
+
+        code_valid_result = invoke_codegen_sequence_with_facts(facts_with_valid_clash)
+        self.assertEquals(code_valid_result, satisfiable)
+        code_invalid_result = invoke_codegen_sequence_with_facts(facts_with_invalid_clash)
+        self.assertEquals(code_invalid_result, unsatisfiable)
+        pass
+
+    def test_students_no_more_than_6_hours_a_day(self):
+        facts = [ta_models.LectureClass().init_from_json( \
+            generate_lectureclass_json("Hardware", "308", "Monday", 9)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Hardware", "308", "Monday", 10)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Math Methods", "308", "Monday", 12)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Math Methods", "308", "Monday", 13)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Logic", "308", "Monday", 15)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Logic", "308", "Monday", 16)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Logic", "308", "Monday", 17)),
+        ]
+        pass
+
+    def test_lectures_of_same_subject_cant_be_in_2_diferent_rooms_at_same_time(self):
+        facts = [ta_models.LectureClass().init_from_json( \
+            generate_lectureclass_json("Math Methods", "311", "Monday", 10)),
+            ta_models.LectureClass().init_from_json( \
+                generate_lectureclass_json("Math Methods", "308", "Monday", 10)),
+        ]
+
+        code_result = invoke_codegen_sequence_with_facts(facts)
+        self.assertEquals(code_result, unsatisfiable)
