ba7e7750c9c7e9828562305aa471c7277cffe361
Nikolay Yotov <ny1215@ic.ac.uk>
2017-11-22 20:59:54 +0000

    async callback to fill state

--- Frontend/src/client/app/Utils.jsx
+++ Frontend/src/client/app/Utils.jsx
@@ -2,25 +2,25 @@ import React from "react"
 import axios from 'axios'
 
 var utils = {
-  getDropdownData: function(){
+  getDropdownData: function(main_this){
     var data = {};
     axios.get('/choices/terms').
     then((response) => {
-        data.selectable_terms = response.data;
+        main_this.setState({selectable_terms: response.data});
     })
     .catch(function (error) {
       console.log(error);
     });
     axios.get('/choices/subjects').
     then((response)=>{
-      data.subjects = response.data;
+      main_this.setState({subjects: response.data});
     })
     .catch(function(error){
       console.log(error)
     })
     axios.get('/choices/rooms').
     then((response)=>{
-      data.rooms = response.data;
+      main_this.setState({rooms: response.data});
     })
     .catch(function(error){
       console.log(error)
