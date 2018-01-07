9a5efb730a4663d6cc245d5d6227ec8b4dd20037
myx9395 <myx14@ic.ac.uk>
2018-01-07 12:55:03 +0000

    re-styled modals

--- Frontend/src/client/app/Utils.jsx
+++ Frontend/src/client/app/Utils.jsx
@@ -27,6 +27,66 @@ var utils = {
     })
     return data;
   }
+
 }
 
-module.exports = utils
+var styles = {
+  cstyle : {
+    overlay : {
+      position          : 'fixed',
+      top               : 0,
+      left              : 0,
+      right             : 0,
+      bottom            : 0,
+      backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+    },
+    content : {
+      position                   : 'relative',
+      top                        : '40px',
+      left                       : '50%',
+      right                      : '0px',
+      bottom                     : '40px',
+      border                     : '1px solid #ccc',
+      background                 : '#fff',
+      overflow                   : 'auto',
+      WebkitOverflowScrolling    : 'touch',
+      borderRadius               : '4px',
+      outline                    : 'none',
+      padding                    : '20px',
+      width                      : '400px',
+      height                     : '400px',
+      transform: 'translate(-50%, 0)'
+   }
+
+},
+sstyle : {
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+  },
+  content : {
+    position                   : 'relative',
+    top                        : '40px',
+    left                       : '50%',
+    right                      : '0px',
+    bottom                     : '40px',
+    border                     : '1px solid #ccc',
+    background                 : '#fff',
+    overflow                   : 'auto',
+    WebkitOverflowScrolling    : 'touch',
+    borderRadius               : '4px',
+    outline                    : 'none',
+    padding                    : '20px',
+    width                      : '400px',
+    height                     : '200px',
+    transform: 'translate(-50%, 0)'
+ }
+}
+}
+
+export {utils}
+export {styles}
