16055bacf20e8aaba2651d8f80f6e5216bfc8731
myx9395 <myx14@ic.ac.uk>
2018-01-07 14:58:07 +0000

    fixed some styling issues on timetable

--- Frontend/src/client/app/TimetableSlot.jsx
+++ Frontend/src/client/app/TimetableSlot.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import {render} from 'react-dom';
 
   const TimetableSlot = (props) => {
+
     return(
-      <div>
+      <div className="timeslot">
       {props.name}
       <br/>
       {props.room}
